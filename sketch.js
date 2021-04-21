c7d1e9c7c069a6c14c791ff1888a60e86a00a1b6
charan2006 <77147803+charan2006@users.noreply.github.com>
2021-04-21 19:21:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var b1;
+
+function preload(){
+  
+
+}
+function setup(){
+  createCanvas(700,450);
+  background("black");
+  b1 = createSprite(200,200,20,20);
+  b1.shapeColor = "white"
+ 
+
+}
+function draw(){
+ if(mousePressedOver(b1)){
+  b1.shapeColor = "blue";
+ }
+ if (this.Touch[b1]){
+  b1.shapeColor = "blue";
+ }
+  drawSprites();
+}
